3bb44ec86930d3fee2feabfd0cae03a5750c9651
Alexander <AlexOpryshko@yandex.ru>
2014-10-06 23:43:07 +0400

    test to notification_pusher (with out main and main_loop)

--- source/tests/test_notification_pusher.py
+++ source/tests/test_notification_pusher.py
@@ -1,9 +1,40 @@
 import unittest
 import mock
-from notification_pusher import notification_worker
+from notification_pusher import notification_worker, done_with_processed_tasks, install_signal_handlers, main
+
+
+class MockConfig:
+    def __init__(self):
+        self.field = mock.Mock()
+
+    def __getattr__(self, item):
+        return self.field
+
+
+def stop(*args, **kwargs):
+    import source.notification_pusher as notification_pusher
+    notification_pusher.run_application = False
 
 
 class NotificationPusherTestCase(unittest.TestCase):
+    def setUp(self):
+        import source.notification_pusher as notification_pusher
+        #notification_pusher.run_application = True
+        from lib.utils import Config
+        self.config = Config()
+        self.config.QUEUE_HOST = 'localhost'
+        self.config.QUEUE_PORT = 33013
+        self.config.QUEUE_SPACE = 0
+        self.config.QUEUE_TAKE_TIMEOUT = 0.1
+        self.config.QUEUE_TUBE = 'api.push_notifications'
+
+        self.config.HTTP_CONNECTION_TIMEOUT = 30
+        self.config.SLEEP = 0.1
+        self.config.SLEEP_ON_FAIL = 10
+
+        self.config.WORKER_POOL_SIZE = 1
+        notification_pusher.sleep = mock.Mock(side_effect=stop)
+
     @mock.patch('source.notification_pusher.current_thread', mock.Mock())
     @mock.patch('source.notification_pusher.requests.post', mock.Mock(return_value=mock.Mock()))
     @mock.patch('source.notification_pusher.json.dumps', mock.Mock())
@@ -35,5 +66,51 @@ class NotificationPusherTestCase(unittest.TestCase):
             notification_worker(m_task, m_task_queue)
         m_task_queue.put.assert_called_once_with((m_task, 'bury'))
 
-    
+    def test_done_with_processed_tasks(self):
+        m_task_queue = mock.MagicMock()
+        m_task_queue.qsize.return_value = 1
+        m_task = mock.MagicMock()
+        m_task.task_id = 1
+        m_task_queue.get_nowait.return_value = (m_task, 'action_name.capitalize')
+        done_with_processed_tasks(m_task_queue)
+        m_task_queue.get_nowait.assert_called_once_with()
+
+    @mock.patch('source.notification_pusher.logger.debug')
+    def test_done_with_processed_tasks_empty_exception(self, m_logger):
+        m_task_queue = mock.MagicMock()
+        m_task_queue.qsize.return_value = 1
+        from gevent import queue as gevent_queue
+        m_task_queue.get_nowait.side_effect = gevent_queue.Empty
+        done_with_processed_tasks(m_task_queue)
+        self.assertTrue(m_logger.called)
+
+    @mock.patch('source.notification_pusher.logger.exception')
+    def test_done_with_processed_tasks_tarantool_database_error(self, m_logger):
+        m_task_queue = mock.MagicMock()
+        m_task_queue.qsize.return_value = 1
+        m_task = mock.MagicMock()
+        m_task.task_id = 1
+        m_task_queue.get_nowait.return_value = (m_task, 'action_name')
+        import tarantool
+        m_task.action_name.side_effect = tarantool.DatabaseError
+        done_with_processed_tasks(m_task_queue)
+        self.assertTrue(m_logger.called)
+
+    @mock.patch('source.notification_pusher.stop_handler', mock.Mock())
+    @mock.patch('source.notification_pusher.gevent.signal')
+    def test_install_signal_handlers(self, m_gevent_signal):
+        import signal
+        signals = [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]
+        install_signal_handlers()
+        self.assertEqual(m_gevent_signal.call_count, len(signals))
+
+    @mock.patch('source.notification_pusher.current_thread', mock.Mock())
+    @mock.patch('source.notification_pusher.run_application', True)
+    @mock.patch('source.notification_pusher.exit_code', 0)
+    def test_stop_handler(self):
+        import source.notification_pusher as notification_pusher
+        sig = 1
+        notification_pusher.stop_handler(sig)
+        self.assertEqual(notification_pusher.run_application, False)
+        self.assertEqual(notification_pusher.exit_code, notification_pusher.SIGNAL_EXIT_CODE_OFFSET + sig)
 
