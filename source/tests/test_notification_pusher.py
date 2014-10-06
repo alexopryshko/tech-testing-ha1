9e60d319a7050b48d35d93b78dd35e4bc7c73a2a
Alexander <AlexOpryshko@yandex.ru>
2014-10-06 19:39:55 +0400

    first test to notification_pusher

--- source/tests/test_notification_pusher.py
+++ source/tests/test_notification_pusher.py
@@ -1,15 +1,39 @@
 import unittest
 import mock
-from notification_pusher import create_pidfile
+from notification_pusher import notification_worker
 
 
 class NotificationPusherTestCase(unittest.TestCase):
-    def test_create_pidfile_example(self):
-        pid = 42
-        m_open = mock.mock_open()
-        with mock.patch('notification_pusher.open', m_open, create=True):
-            with mock.patch('os.getpid', mock.Mock(return_value=pid)):
-                create_pidfile('/file/path')
+    @mock.patch('source.notification_pusher.current_thread', mock.Mock())
+    @mock.patch('source.notification_pusher.requests.post', mock.Mock(return_value=mock.Mock()))
+    @mock.patch('source.notification_pusher.json.dumps', mock.Mock())
+    def test_notification_worker_ok(self):
+        m_task_queue = mock.MagicMock()
+        m_task = mock.MagicMock()
+        data = {
+            'id': 1,
+            'callback_url': 'callback_url'
+        }
+        m_task.data.copy.return_value = data
+        m_task_queue.task_id = 1
+        notification_worker(m_task, m_task_queue)
+        m_task_queue.put.assert_called_once_with((m_task, 'ack'))
 
-        m_open.assert_called_once_with('/file/path', 'w')
-        m_open().write.assert_called_once_with(str(pid))
+    @mock.patch('source.notification_pusher.current_thread', mock.Mock())
+    @mock.patch('source.notification_pusher.json.dumps', mock.Mock())
+    def test_notification_worker_request_exception(self):
+        m_task_queue = mock.MagicMock()
+        m_task = mock.MagicMock()
+        data = {
+            'id': 1,
+            'callback_url': 'callback_url'
+        }
+        m_task.data.copy.return_value = data
+        m_task_queue.task_id = 1
+        import requests
+        with mock.patch('source.notification_pusher.requests.post', mock.Mock(side_effect=requests.RequestException)):
+            notification_worker(m_task, m_task_queue)
+        m_task_queue.put.assert_called_once_with((m_task, 'bury'))
+
+    
+
