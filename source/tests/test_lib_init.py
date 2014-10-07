53fb0d4aa953e3b9fa760d09f39bcfb4b021161e
Рубцов Дмитрий <d.rubtsov@corp.mail.ru>
2014-10-07 16:43:30 +0400

    last init tests

--- source/tests/test_lib_init.py
+++ source/tests/test_lib_init.py
@@ -5,13 +5,13 @@ import rstr
 from source.lib import check_for_meta, \
     make_pycurl_request, \
     fix_market_url, \
-    get_url, REDIRECT_HTTP, COUNTER_TYPES, get_counters, get_redirect_history, prepare_url
+    get_url, REDIRECT_HTTP, COUNTER_TYPES, get_counters, get_redirect_history, prepare_url, REDIRECT_META, prepare_url
 
 
 class LibInitCase(unittest.TestCase):
 
     def test_check_for_meta(self):
-        meta = '<html><head><meta http-equiv="refresh" content="0;URL=''http://thetudors.example.com/''" /></head></html>'
+        meta = self._get_html_with_redirect('http://thetudors.example.com/')
         self.assertEqual(check_for_meta(meta, ''), 'http://thetudors.example.com/')
 
     def test_check_for_bad_meta(self):
@@ -65,12 +65,12 @@ class LibInitCase(unittest.TestCase):
         self.assertEqual(len(counters), len(COUNTER_TYPES))
 
     def test_get_redirect_history(self):
-        m_types = ['meta_tag', 'meta_tag']
+        m_types = [REDIRECT_META, REDIRECT_META]
         m_urls = ['url1', 'url2', 'url3']
 
         with mock.patch('source.lib.get_url', mock.Mock(side_effect=[
-            (m_urls[0], m_types[0], '<html></html>'),
-            (m_urls[1], m_types[1], '<html></html>'),
+            (m_urls[1], m_types[0], self._get_html_with_redirect(m_urls[1])),
+            (m_urls[2], m_types[1], self._get_html_with_redirect(m_urls[2])),
             (None, None, '<html></html>')
         ])):
             types, urls, counters = get_redirect_history(m_urls[0], timeout=11)
@@ -83,18 +83,80 @@ class LibInitCase(unittest.TestCase):
 
             self.assertEquals(redirect_type, 'ERROR')
 
-    def prepare_none_url(self):
+    def test_prepare_none_url(self):
         self.assertEqual(None, prepare_url(None))
 
+    def _get_html_with_redirect(self, url):
+        return '<html><head><meta http-equiv="refresh" content="0;URL=''{0}''" /></head></html>'.format(url)
+
     def test_get_redirect_history_ok(self):
-        m_types = ['meta_tag', 'meta_tag']
-        m_urls = ['url1', 'http://odnoklassniki.ru/', 'url3']
+        m_types = [REDIRECT_META, REDIRECT_META]
+        m_urls = ['http://odnoklassniki.ru/', 'url1', 'url3']
 
         with mock.patch('source.lib.get_url', mock.Mock(side_effect=[
-            (m_urls[1], m_types[0], '<html></html>'),
-            (m_urls[0], m_types[1], '<html></html>'),
+            (m_urls[1], m_types[0], self._get_html_with_redirect(m_urls[1])),
+            (m_urls[2], m_types[1], self._get_html_with_redirect(m_urls[2])),
             (None, None, '<html></html>')
         ])):
             types, urls, counters = get_redirect_history(m_urls[0], timeout=11)
 
             self.assertEquals(len(urls), 1)
+
+    def test_get_url_redirect(self):
+        with mock.patch('source.lib.make_pycurl_request',  mock.Mock(return_value=(self._get_html_with_redirect(
+        'http://odnoklassniki.ru/st.redirect'), None))):
+            url, type, content = get_url('http://odnoklassniki.ru/st.redirect', 11)
+            self.assertEqual(type, REDIRECT_META)
+
+    def test_prepare_url_exception(self):
+        m_netlock = mock.MagicMock()
+        m_netlock.encode = mock.Mock(side_effect=UnicodeError)
+        m_urlparse = mock.Mock(return_value=('', m_netlock, '', '', '', ''))
+
+        with mock.patch('source.lib.urlparse', m_urlparse):
+            try:
+                prepare_url('url')
+            except UnicodeError:
+                self.fail()
+
+    def test_check_for_meta_wrong(self):
+        self.assertEqual(check_for_meta('', ''), None)
+
+    def test_check_for_meta_wrong_url(self):
+        self.assertEqual(
+            check_for_meta('<html><head><meta http-equiv="refresh" content="0;URwqeL=''http://thetudors.example.com/''" /></head></html>', ''),
+            None
+        )
+
+    def test_get_url_ok_login(self):
+        with mock.patch('source.lib.make_pycurl_request',  mock.Mock(return_value=(self._get_html_with_redirect(
+        'http://odnoklassniki.ru/st.redirect'), 'http://odnoklassniki.ru/st.redirect'))):
+            url, type, content = get_url('http://odnoklassniki.ru/st.redirect', 11)
+            self.assertEqual(type, None)
+
+    def test_get_url_market(self):
+        with mock.patch('source.lib.make_pycurl_request',
+                        mock.Mock(return_value=
+                        (self._get_html_with_redirect('market://url/index.html'), None)
+                        )),\
+            mock.patch('source.lib.fix_market_url') as m_fix_market:
+            get_url('market://url', 11)
+
+            self.assertTrue(m_fix_market.called)
+
+    def test_fix_market_absolute(self):
+        self.assertEqual(fix_market_url('path'), 'http://play.google.com/store/apps/path')
+
+    def test_get_redirect_history_error(self):
+        m_types = ['ERROR', REDIRECT_META]
+        m_urls = ['http://url', 'http://url2', 'http://url3']
+
+        with mock.patch('source.lib.get_url', mock.Mock(side_effect=[
+            (m_urls[1], m_types[0], self._get_html_with_redirect('http://url2')),
+            (m_urls[2], m_types[1], self._get_html_with_redirect('http://url3')),
+            (None, None, '<html></html>')
+        ])):
+            history_types, history_urls, counters = get_redirect_history(m_urls[0], 11)
+            self.assertEquals(len(history_urls), 2)
+
+
